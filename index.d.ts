34f850e4b46185fde4805b83dd1da26d3bf30135
netanel-mce <netanel.d@mce.systemd>
2022-11-02 10:28:29 +0200

    types for some and fix types for all

--- index.d.ts
+++ index.d.ts
@@ -54,6 +54,8 @@ interface InterfaceEntry<Interfaces, K extends keyof Interfaces, J extends Jarvi
 	purge: Function;
 }
 
+type ExtractJarvis<J extends JarvisEmitter<any, any>> = J extends JarvisEmitter<infer D, infer E> ? { d: D; e: E } : { d: void; e: Error };
+
 declare class JarvisEmitter<DoneType = void, ErrorType = Error, Interfaces = DefaultInterfaces<DoneType, ErrorType>> {
 	constructor();
 	on: {
@@ -74,7 +76,8 @@ declare class JarvisEmitter<DoneType = void, ErrorType = Error, Interfaces = Def
 	getRolesHandlers(role: Role): InterfaceEntry<Interfaces, keyof Interfaces, JarvisEmitter<DoneType, ErrorType, Interfaces>>[];
 	getHandlersForName<T extends keyof Interfaces>(name: T, role?: string): InterfaceEntry<Interfaces, T, JarvisEmitter<DoneType, ErrorType, Interfaces>>;
 	destroy(): void;
-	static all<J extends JarvisEmitter<any, any>[]>(...emitters: J): JarvisEmitter<Array<J[number] extends JarvisEmitter<infer D> ? D : void>, J[number] extends JarvisEmitter<any, infer E> ? E : Error>;
+	static some<J extends Array<JarvisEmitter<any, any>>>(...emitters: J): JarvisEmitter<Array<ExtractJarvis<J[number]>["d"] | undefined>, never>;
+	static all<J extends JarvisEmitter<any, any>[]>(...emitters: J): JarvisEmitter<Array<ExtractJarvis<J[number]>["d"]>, ExtractJarvis<J[number]>["e"]>;
 	static emitifyFromAsync<I extends any[], O>(fn: (...args: I) => Promise<O>): (...callArgs: I) => JarvisEmitter<O, any>;
 }
 
